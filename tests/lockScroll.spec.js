7a9d0f5ada0901bebcc5f1e473cd4e630a56069f
Alexey Raspopov <alexey.raspopov@datarobot.com>
2021-10-21 20:32:44 -0400

    use boundingBox

--- tests/lockScroll.spec.js
+++ tests/lockScroll.spec.js
@@ -6,27 +6,27 @@ test('scroll lock toggle', async ({ page }) => {
   let toggle = page.locator('#toggle');
 
   // ensure the page is not scrolled
-  let initialPosition = await block.evaluate((node) => node.getBoundingClientRect().top);
+  let { y: initialPosition } = await block.boundingBox();
   expect(initialPosition).toBe(0);
 
   // first, scroll page by 50px
   await page.mouse.wheel(0, 50);
   await page.waitForTimeout(50);
-  let scrolledPosition = await block.evaluate((node) => node.getBoundingClientRect().top);
+  let { y: scrolledPosition } = await block.boundingBox();
   expect(scrolledPosition).toBe(-50);
 
   // lock body scroll, try scrolling page
   await toggle.click();
   await page.mouse.wheel(0, 100);
   await page.waitForTimeout(50);
-  let lockedPosition = await block.evaluate((node) => node.getBoundingClientRect().top);
+  let { y: lockedPosition } = await block.boundingBox();
   expect(lockedPosition).toBe(scrolledPosition);
 
   // unlock body scroll, try scrolling page
   await toggle.click();
   await page.mouse.wheel(0, 150);
   await page.waitForTimeout(50);
-  let renewedPosition = await block.evaluate((node) => node.getBoundingClientRect().top);
+  let { y: renewedPosition } = await block.boundingBox();
   expect(renewedPosition).not.toBe(scrolledPosition);
   expect(renewedPosition).toBe(-200);
 });
